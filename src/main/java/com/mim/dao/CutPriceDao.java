2922db9eaa25b622c23b2d6c853b722d1ee96cc5
益达MIM味 <794391017@qq.com>
2019-09-01 14:40:48 +0800

    数据源分片策略使用单独yml文件配置

--- src/main/java/com/mim/dao/CutPriceDao.java
+++ src/main/java/com/mim/dao/CutPriceDao.java
@@ -13,8 +13,8 @@ import java.util.List;
 @Mapper
 public interface CutPriceDao {
 
-    //@Insert("insert into t_cut_price (luck,created_by,created_date) values (#{luck},#{createdBy},SYSDATE())")
-    @Insert("insert into t_cut_price(created_by,created_date) values (#{createdBy},SYSDATE())")
+    @Insert("insert into t_cut_price (luck,created_by,created_date) values (#{luck},#{createdBy},SYSDATE())")
+    //@Insert("insert into t_cut_price(created_by,created_date) values (#{createdBy},SYSDATE())")
     int addCutPrice(CutPrice o);
 
     @Select("select id,luck,created_by  createdBy,created_date createdDate from t_cut_price")
